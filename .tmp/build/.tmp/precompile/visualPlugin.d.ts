d7026ac3ae35f60668adc6d50fc512d25eb9e876
DanielSzentimrey-Harrach <daniel.szhm@gmail.com>
2024-08-02 15:46:38 +0800

    Remove files that are now excluded by .gitignore

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var enumTestVisualE2C307D96B50414FA2D568DD51AE3B52_DEBUG: IVisualPlugin;
-export default enumTestVisualE2C307D96B50414FA2D568DD51AE3B52_DEBUG;
--- .tmp/build/src/singleton.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Singleton {
-    private static instance;
-    values: any[];
-    private constructor();
-    static getInstance(): Singleton;
-}
-export declare const singletonInstance: Singleton;
-export {};
